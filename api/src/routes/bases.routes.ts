89736ff42d755117a1d23d90ea109d526a76cac9
llOrtegall <IvanOrtega_97@hotmail.com>
2024-07-04 11:33:50 -0500

    mandar a producttion

--- api/src/routes/bases.routes.ts
+++ api/src/routes/bases.routes.ts
@@ -1,4 +1,4 @@
-import { getAllBases, getBaseDatalle, updateBase, usersSinBase, crearBase } from '../controllers/bases.controller'
+import { getAllBases, getBaseDatalle, updateBase, usersSinBase, crearBase, basesInfoUpdates } from '../controllers/bases.controller'
 import { Router } from 'express' 
 
 export const BasesRouter = Router()
@@ -12,3 +12,5 @@ BasesRouter.get('/usersSinBase', usersSinBase)
 BasesRouter.post('/updateBase', updateBase)
 
 BasesRouter.post('/asignar-base', crearBase)
+
+BasesRouter.get('/updatesBases/:id', basesInfoUpdates)
