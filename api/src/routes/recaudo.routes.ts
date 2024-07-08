ee829c6d3fba3406371964b9fbd3ca5a80f740df
llOrtegall <IvanOrtega_97@hotmail.com>
2024-07-08 09:03:43 -0500

    router creada

---
+++ api/src/routes/recaudo.routes.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { getRecaudo } from "../controllers/recaudo.controller";
+
+export const recaudoRouter = Router();
+
+recaudoRouter.get('/recaudo', getRecaudo);
