76855648389efdcb5c0f5565b29fa075809f6abc
llOrtegall <IvanOrtega_97@hotmail.com>
2024-07-08 09:02:57 -0500

    controller creado

---
+++ api/src/controllers/recaudo.controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import { Recaudo } from '../model'
+import { fn } from 'sequelize';
+
+export const getRecaudo = async (req: Request, res: Response) => {
+  const { id } = req.query;
+
+  try {
+    await Recaudo.sync();
+
+    const result = await Recaudo.findOne({
+      where: {
+        VINCULADO: id as string,
+        FECHA: fn('CURDATE')
+      }
+    })
+    
+    if(!result) return res.status(404).json({ message: 'No se encontraron datos' });
+
+    return res.status(200).json(result);
+  } catch (error) {
+    console.error('Error en getRecaudo', error);
+    return res.status(500).json({ message: 'Error en getRecaudo' });
+  }
+}
