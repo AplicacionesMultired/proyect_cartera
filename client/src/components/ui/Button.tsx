7da2a677c0852f9c6df2229a23b85814f2113a6b
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-07-31 15:25:15 -0500

    organizar toast

--- client/src/components/ui/Button.tsx
+++ client/src/components/ui/Button.tsx
@@ -4,7 +4,7 @@ export function Button ({ children, disabled, ...props }: Props): JSX.Element {
   return (
     <button
       type="submit"
-      className={`text-xs text-white rounded-md px-4 py-2 bg-gradient-to-b from-punch-700 to-punch-800 hover:bg-gradient-to-b
+      className={`text-xs 2xl:text-2xl text-white rounded-md px-4 py-2 bg-gradient-to-b from-punch-700 to-punch-800 hover:bg-gradient-to-b
       hover:from-punch-500 hover:to-punch-600 transition-all ease-in-out ${
         disabled ? 'opacity-50 cursor-not-allowed' : ''}`}
       disabled={disabled}
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,44 +1,50 @@
 import { UserIcon, LockIcon } from '../components/icons'
 import { Input, Button, Label } from '../components/ui'
-import { Error } from '../components/ui/Error'
 import { useLogin } from '../hooks/useLogin'
+import { toast, Toaster } from 'sonner'
 
 function LoginPage (): JSX.Element {
   const { user, setUser, password, setPassword, errorString, handleSubmit } = useLogin()
 
   return (
-    <section className="h-[100vh] flex flex-col items-center justify-center bg-gradient-to-b from-punch-300 to-punch-200">
-      <form className='w-[385px] mb-2 border p-12 rounded-lg bg-white/30 flex flex-col gap-6 shadow-xl' onSubmit={handleSubmit}>
-        <figure className='flex items-center justify-center'>
-          <img src="/gane.webp" alt="logo de gane" className='w-[160px] ' />
-        </figure>
-        <article className='flex flex-col gap-1'>
-          <Label>Usuario: </Label>
-          <div className='flex items-center gap-2 w-full justify-around px-2'>
-            <UserIcon />
-            <Input name='username' type='text' placeholder='CP1118342523' required
-            autoComplete='username' value={user}
-              onChange={(ev) => { setUser(ev.target.value) }} />
-          </div>
-        </article>
+    <section className="w-screen h-screen flex bg-gradient-to-t from-punch-200 to-gray-100">
+      <figure className='w-full'>
+        <img src="logo.webp" alt="logo para cartera" className='h-full' />
+      </figure>
 
-        <article className='flex flex-col gap-1'>
-          <Label>Contraseña:</Label>
-          <div className='flex items-center gap-2 w-full justify-around px-2'>
-            <LockIcon />
-            <Input name='contraseña' type='password' placeholder='***********' required
-            autoComplete='contraseña' value={password}
-              onChange={(ev) => { setPassword(ev.target.value) }} />
-          </div>
-        </article>
+      <section className='w-full grid place-content-center'>
+        <form className='min-w-96 flex flex-col gap-8' onSubmit={handleSubmit}>
+          <figure className='flex items-center justify-center'>
+            <img src="/gane.webp" alt="logo de gane" className='w-[220px] ' />
+          </figure>
+          <article className='flex flex-col gap-1 text-md lg:text-lg 2xl:text-2xl'>
+            <Label>Usuario: </Label>
+            <div className='flex items-center gap-2 w-full justify-around px-2'>
+              <UserIcon />
+              <Input name='username' type='text' placeholder='CP1118342523' required
+                autoComplete='username' value={user}
+                onChange={(ev) => { setUser(ev.target.value) }} />
+            </div>
+          </article>
 
-        <article className='flex w-full items-center justify-center'>
+          <article className='flex flex-col gap-1 text-md lg:text-lg 2xl:text-2xl'>
+            <Label>Contraseña:</Label>
+            <div className='flex items-center gap-2 w-full justify-around px-2'>
+              <LockIcon />
+              <Input name='contraseña' type='password' placeholder='***********' required
+                autoComplete='contraseña' value={password}
+                onChange={(ev) => { setPassword(ev.target.value) }} />
+            </div>
+          </article>
+
           <Button>Iniciar Sesión</Button>
-        </article>
 
-      </form >
+        </form >
+      </section>
 
-      <Error errorString={errorString} />
+      {errorString && toast.error(errorString)}
+
+      <Toaster />
 
     </section >
   )
