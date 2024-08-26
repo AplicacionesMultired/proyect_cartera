7fcf2beb7f8ad6e391f6e00655eefdd854d04ed8
llOrtegall <IvanOrtega_97@hotmail.com>
2024-08-26 10:35:16 -0500

    refactor: Update BasesPage to use 'aux cartera' instead of 'cartera' for user sub_process

--- client/src/types/user.d.ts
+++ client/src/types/user.d.ts
@@ -1,10 +1,11 @@
 export interface User {
-  id: string;
-  names: string,
-  lastnames: string,
-  username: string,
-  email: string,
-  company: string,
-  process: string,
-  sub_process: string,
+  id: string
+  names: string
+  lastnames: string
+  username: string
+  email: string
+  company: string
+  process: string
+  sub_process: string
+  app: string
 }
